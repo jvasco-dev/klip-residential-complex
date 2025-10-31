2a1dcc11e0c33064a21d1d501d4443c5a4448ca1
Jorge Vasco <jvascov@gmail.com>
2025-10-30 21:42:00 -0500

    Initial commit. -postgres typeorm config.env uuid class validator and class transformer

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '',
+      port: +'',
+      database: '',
+      username: '',
+      password: '',
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
